8a132ce58963c3a7254ef92ead33fcf2ef3ce978
NicoAmendo <nicoamedolara@gmail.com>
2020-05-27 23:44:58 -0300

    levanto servidor

--- site/app.js
+++ site/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.listen(3000, () => console.log("servidor corriendo"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
