b3b863c255bba04b78717deb46b190a443370e9c
NicoAmendo <nicoamendolara@gmail.com>
2020-06-16 22:27:48 -0300

    formularioDeCarga

--- site/Controllers/formularioCargaController.js
+++ site/Controllers/formularioCargaController.js
@@ -1,8 +1,52 @@
 const formularioCargaController = {
     principal: (req,res) => {
         res.render('formularioDeCarga');
-    }
-};
+    
+},
 
+store: (req, res) => {
+    // Do the magic
+    let nuevoProducto={}
+    if(products==""){
+        nuevoProducto.id=1
+    } else { 
+    let ultimoProducto=products[products.length-1]
+    nuevoProducto.id=ultimoProducto.id+1
+    }
+    
+    nuevoProducto.nombre=req.body.nombre
+    nuevoProducto.price=req.body.price
+    nuevoProducto.mensaje=req.body.mensaje
+    nuevoProducto.categoria=req.body.categoria
+    nuevoProducto.imagen=req.body.imagen
+    
+    //res.send(nuevoProducto)
+    products.push(nuevoProducto)
+
+    let productosModificadosJSON = JSON.stringify(products)
+    fs.writeFileSync(productsFilePath, productosModificadosJSON)
+
+    },
+        /*res.render("addProduct");/*
+
+
+//create -Method to store//
+/*store:(req,res,next)=>{
+    req.body.price = Number(req.body.price);
+    req.body.nombre = Number(req.body.nombre);
+
+    let newProduct = {
+        id: products [products.length -1]. id + 1,
+        ...req.body,
+        image: "default-image.png"
+    };
+    let finalProducts = [...products, newProduct];
+    fs. writeFileSync(productsFilePath, JSON.stringify(finalProduct,null, " "));
+    res.redirect("/");
+    
+
+ 
+}*/
+};
 
 module.exports = formularioCargaController;
--- site/routes/formularioCarga.js
+++ site/routes/formularioCarga.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+
 const formularioCargaController = require('../controllers/formularioCargaController')
 
-/* GET home page. */
-router.get('/', formularioCargaController.principal);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null,"./public/images/products");
 
-router.post("/", (req, res) =>{
-    console.log(req.body.nombre);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + " ");
+
+    } 
+
+});
+const upload = multer({ storage: storage})
+
+/* GET home page. */
+router.get("/", formularioCargaController.principal);
+//router.post("/Create", upload.any(), formularioCargaController.store);
+
+router.post("/site/views/formularioDeCarga.ejs", (req, res) =>{
+    console.log(req.body);
     res.send("recibido");
 });
 module.exports = router;
