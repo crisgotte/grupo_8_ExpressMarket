1fe18414ab7eb798e8c755fd98da4fc9456d6588
NicoAmendo <62446010+NicoAmendo@users.noreply.github.com>
2020-08-08 17:30:49 -0300

    agregar al carrito

---
+++ site/views/agregaralcarrito.js
@@ -0,0 +1,7 @@
+window.addEventListener("load",function(){
+    let buttonAgregar = document.querySelector(".btn btn-warning");
+
+    buttonAgregar.addEventListener("click" , function(){ 
+        buttonAgregar.backgroundcolor = "black";
+    }),
+})
